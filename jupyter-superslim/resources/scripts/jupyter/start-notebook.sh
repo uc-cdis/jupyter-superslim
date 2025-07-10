d6f6c1c37b4a32a2d554c6b39fff7061d736a2b3
George Thomas <george42@uchicago.edu>
2025-07-10 10:05:53 -0700

    (HP-2178) revert 'flock' fixes, remove 'RESTARTABLE', move entrypoint

--- jupyter-superslim/resources/scripts/jupyter/start-notebook.sh
+++ jupyter-superslim/resources/scripts/jupyter/start-notebook.sh
@@ -8,25 +8,10 @@ if [[ -n "${JUPYTERHUB_API_TOKEN}" ]]; then
     # launched by JupyterHub, use single-user entrypoint
     exec /usr/local/bin/start-singleuser.sh "$@"
 elif [[ -n "${JUPYTER_ENABLE_LAB}" ]]; then
-
-    if [[ "${RESTARTABLE}" == "yes" ]]; then
-        while true; do
-            # shellcheck disable=SC1091
-            flock -n /tmp/jupyter_notebook.lock . /usr/local/bin/start.sh jupyter lab -y "$@"
-        done
-    else
-        # shellcheck disable=SC1091
-        . /usr/local/bin/start.sh jupyter lab -y "$@"
-    fi
+    # shellcheck disable=SC1091
+    . /usr/local/bin/start.sh jupyter lab -y "$@"
 else
     echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
-    if [[ "${RESTARTABLE}" == "yes" ]]; then
-        while true; do
-            # shellcheck disable=SC1091
-            flock -n /tmp/jupyter_notebook.lock . /usr/local/bin/start.sh jupyter notebook -y "$@"
-        done
-    else
-        # shellcheck disable=SC1091
-        . /usr/local/bin/start.sh jupyter notebook -y "$@"
-    fi
+    # shellcheck disable=SC1091
+    . /usr/local/bin/start.sh jupyter notebook -y "$@"
 fi
