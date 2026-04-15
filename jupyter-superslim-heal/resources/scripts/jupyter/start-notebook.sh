50ad4b63924f1ca21490b9d8816d157361bc7e26
J Maxwell <jmontmaxwell@uchicago.edu>
2026-04-15 12:08:12 -0500

    start heal base image

---
+++ jupyter-superslim-heal/resources/scripts/jupyter/start-notebook.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -e
+
+if [[ -n "${JUPYTERHUB_API_TOKEN}" ]]; then
+    # launched by JupyterHub, use single-user entrypoint
+    exec /usr/local/bin/start-singleuser.sh "$@"
+elif [[ -n "${JUPYTER_ENABLE_LAB}" ]]; then
+    # shellcheck disable=SC1091
+    . /usr/local/bin/start.sh jupyter lab -y "$@"
+else
+    echo "WARN: Jupyter Notebook deprecation notice https://github.com/jupyter/docker-stacks#jupyter-notebook-deprecation-notice."
+    # shellcheck disable=SC1091
+    . /usr/local/bin/start.sh jupyter notebook -y "$@"
+fi
